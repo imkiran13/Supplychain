8599b53fd2af11718dbc672b63a79ce4288922fd
kiran <imkiran13@gmail.com>
2023-11-06 11:12:41 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+$('.owl-carousel').owlCarousel({
+  center: true,
+  items:2,
+  loop:true,
+  dots:true,
+  nav:true,
+  margin:30,
+  responsive:{
+      600:{
+          items:2
+      }
+  }
+});
